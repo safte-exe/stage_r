484707c63a0f2b1f6882768cda59e77f337a6fe0
safte-exe <saftehami@gmail.com>
2025-06-04 15:13:41 +0200

    11 sec avec tables de hachage pour l'Exemple TM yay !!!

--- def_output_exemple/main1.c
+++ def_output_exemple/main1.c
@@ -9,9 +9,9 @@
 #define NB_VAR 1
 #define nb_actions 3
 
+// Definition du LTS de base 
 char* etats[max_etats];
 int num_etats;
-
 char* actions[max_actions];
 
 typedef struct Transition {
@@ -23,46 +23,71 @@ Transition* transitions[max_etats];
 int nb_trans_par_etat[max_etats] = {0};
 
 void init_lts() {
-    etats[0] = "up";
-    etats[1] = "left";
-    etats[2] = "right";
-    etats[3] = "down";
-    num_etats = 4;
-    actions[0] = "a";
-    actions[1] = "b";
-    actions[2] = "c";
+    etats[0] = "l0l1"; etats[1] = "l1l3"; etats[2] = "l0l40";
+    etats[3] = "l2l3"; etats[4] = "l0l3"; etats[5] = "l1l4";
+    etats[6] = "l0l3"; etats[7] = "l2l4"; etats[8] = "l0l41";
+    etats[9] = "l1l3"; etats[10] = "l2l3";
 
-    transitions[0] = malloc(2 * sizeof(Transition));
-    transitions[0][0] = (Transition){.etat_in = 1, .label_action = 0};
-    transitions[0][1] = (Transition){.etat_in = 2, .label_action = 1};
-    nb_trans_par_etat[0] = 2;
+    num_etats = 11;
+    actions[0] = "a"; actions[1] = "b"; actions[2] = "c";
 
-    transitions[1] = malloc(1 * sizeof(Transition));
-    transitions[1][0] = (Transition){.etat_in = 3, .label_action = 1};
-    nb_trans_par_etat[1] = 1;
+    transitions[0] = malloc(3 * sizeof(Transition));
+    transitions[0][0] = (Transition){1, 0}; transitions[0][1] = (Transition){2, 1}; transitions[0][2] = (Transition){3, 0};
+    nb_trans_par_etat[0] = 3;
 
-    transitions[2] = malloc(1 * sizeof(Transition));
-    transitions[2][0] = (Transition){.etat_in = 3, .label_action = 0};
-    nb_trans_par_etat[2] = 1;
+    transitions[1] = malloc(2 * sizeof(Transition));
+    transitions[1][0] = (Transition){4, 2}; transitions[1][1] = (Transition){5, 2};
+    nb_trans_par_etat[1] = 2;
 
-    transitions[3] = malloc(1 * sizeof(Transition));
-    transitions[3][0] = (Transition){.etat_in = 0, .label_action = 2};
-    nb_trans_par_etat[3] = 1;
+    transitions[2] = malloc(3 * sizeof(Transition));
+    transitions[2][0] = (Transition){5, 0}; transitions[2][1] = (Transition){6, 1}; transitions[2][2] = (Transition){7, 0};
+    nb_trans_par_etat[2] = 3;
+
+    transitions[3] = malloc(2 * sizeof(Transition));
+    transitions[3][0] = (Transition){4, 1}; transitions[3][1] = (Transition){7, 2};
+    nb_trans_par_etat[3] = 2;
+
+    transitions[4] = malloc(1 * sizeof(Transition));
+    transitions[4][0] = (Transition){8, 2};
+    nb_trans_par_etat[4] = 1;
+
+    transitions[5] = malloc(2 * sizeof(Transition));
+    transitions[5][0] = (Transition){8, 2}; transitions[5][1] = (Transition){9, 1};
+    nb_trans_par_etat[5] = 2;
+
+    transitions[6] = malloc(2 * sizeof(Transition));
+    transitions[6][0] = (Transition){9, 0}; transitions[6][1] = (Transition){10, 0};
+    nb_trans_par_etat[6] = 2;
+
+    transitions[7] = malloc(2 * sizeof(Transition));
+    transitions[7][0] = (Transition){8, 1}; transitions[7][1] = (Transition){10, 1};
+    nb_trans_par_etat[7] = 2;
+
+    transitions[8] = malloc(1 * sizeof(Transition));
+    transitions[8][0] = (Transition){0, 1};
+    nb_trans_par_etat[8] = 1;
+
+    transitions[9] = malloc(1 * sizeof(Transition));
+    transitions[9][0] = (Transition){0, 2};
+    nb_trans_par_etat[9] = 1;
+
+    transitions[10] = malloc(1 * sizeof(Transition));
+    transitions[10][0] = (Transition){0, 1};
+    nb_trans_par_etat[10] = 1;
 }
 
+// Définition des variables
 typedef struct Variable {
     int v;
 } Variable;
 
 Variable variable;
 
-void init_variables() {
-    variable.v = 0;
-}
+void init_variables() { variable.v = 0; }
 
-Variable update_a( Variable var ) { if (var.v * 2 <= 10 ) { var.v *= 2;} return var; }
-Variable update_b( Variable var ) { if (var.v + 2 <= 10 ) { var.v += 2;} return var; }
-Variable update_c( Variable var ) { return var; }
+Variable update_a(Variable var) { if (var.v + 2 <= 500000 && var.v + 2 >= -500000) var.v += 2; return var; }
+Variable update_b(Variable var) { if (var.v - 1 <= 500000 && var.v - 1 >= -500000) var.v -= 1; return var; }
+Variable update_c(Variable var) { if (var.v * 2 <= 500000 && var.v * 2 >= -500000) var.v *= 2; return var; }
 
 typedef Variable (*UpdateFunction)(Variable);
 UpdateFunction update_functions[nb_actions];
@@ -73,48 +98,63 @@ void init_update_functions() {
     update_functions[2] = update_c;
 }
 
+// Définition de l'état étendu
 typedef struct Etat_x {
     int etat;
     Variable var;
-    int id_interne;
-    UT_hash_handle hh;
 } Etat_x;
 
-Etat_x *etats_x_hash = NULL;
+Etat_x* etats_x = NULL;
 int nb_etats_x = 0;
+int capacite_etats_x = 0;
 
-int etat_x_cle_hash(int etat, int var_v, Etat_x **res) {
-    Etat_x *s;
-    for (s = etats_x_hash; s != NULL; s = s->hh.next) {
-        if (s->etat == etat && s->var.v == var_v) {
-            *res = s;
-            return 1;
-        }
-    }
-    *res = NULL;
-    return 0;
-}
+// Table de hachage
+typedef struct {
+    int etat;
+    int v;
+} EtatXKey;
+
+typedef struct {
+    EtatXKey key;
+    int index;
+    UT_hash_handle hh;
+} EtatXHash;
+
+EtatXHash* etats_x_hash = NULL;
 
 int ajouter_etat_x(int etat_id, Variable vars) {
-    Etat_x *s;
-    if (!etat_x_cle_hash(etat_id, vars.v, &s)) {
-        s = malloc(sizeof(Etat_x));
-        s->etat = etat_id;
-        s->var = vars;
-        s->id_interne = nb_etats_x;
-        HASH_ADD(hh, etats_x_hash, etat, sizeof(Etat_x) - sizeof(UT_hash_handle), s);
-        nb_etats_x++;
+    EtatXKey key = {etat_id, vars.v};
+    EtatXHash* s;
+
+    HASH_FIND(hh, etats_x_hash, &key, sizeof(EtatXKey), s);
+    if (s) return s->index;
+
+    if (etats_x == NULL) {
+        capacite_etats_x = 2;
+        etats_x = malloc(capacite_etats_x * sizeof(Etat_x));
+    } else if (nb_etats_x == capacite_etats_x) {
+        capacite_etats_x *= 2;
+        etats_x = realloc(etats_x, capacite_etats_x * sizeof(Etat_x));
     }
-    return s->id_interne;
+
+    etats_x[nb_etats_x].etat = etat_id;
+    etats_x[nb_etats_x].var = vars;
+
+    s = malloc(sizeof(EtatXHash));
+    s->key = key;
+    s->index = nb_etats_x;
+    HASH_ADD(hh, etats_x_hash, key, sizeof(EtatXKey), s);
+
+    return nb_etats_x++;
 }
 
+// Transitions étendues
 typedef struct Transition_x {
     int cible;
     int action_id;
 } Transition_x;
 
 Transition_x** transitions_x = NULL;
-int capacite_etats_x = 0;
 
 void init_lts_x() {
     transitions_x = malloc(capacite_etats_x * sizeof(Transition_x*));
@@ -124,20 +164,18 @@ void init_lts_x() {
 }
 
 void appliquer_transition1(int index_source) {
-    Etat_x *s;
-    for (s = etats_x_hash; s != NULL; s = s->hh.next) {
-        if (s->id_interne == index_source) break;
-    }
-    if (!s) return;
+    Etat_x source = etats_x[index_source];
+    int etat_id = source.etat;
 
-    int etat_id = s->etat;
     int nb_transitions = nb_trans_par_etat[etat_id];
     transitions_x[index_source] = malloc(nb_transitions * sizeof(Transition_x));
 
     for (int i = 0; i < nb_transitions; i++) {
         Transition t = transitions[etat_id][i];
-        Variable nouvelles_var = update_functions[t.label_action](s->var);
+
+        Variable nouvelles_var = update_functions[t.label_action](source.var);
         int index_cible = ajouter_etat_x(t.etat_in, nouvelles_var);
+
         transitions_x[index_source][i].cible = index_cible;
         transitions_x[index_source][i].action_id = t.label_action;
     }
@@ -152,37 +190,36 @@ void appliquer_transitions() {
 }
 
 void print_lts_x() {
-    Etat_x *s;
-    for (s = etats_x_hash; s != NULL; s = s->hh.next) {
-        int i = s->id_interne;
+    for (int i = 0; i < nb_etats_x; i++) {
         printf("Etat etendu ID #%d\n", i);
-        printf("  Etat de base    : %s (ID %d)\n", etats[s->etat], s->etat);
-        printf("  Variables       : v = %d\n", s->var.v);
-        printf("  Transitions sortantes :\n");
+        printf("\t Etat de base    : %s (ID %d)\n", etats[etats_x[i].etat], etats_x[i].etat);
+        printf("\t Variables       : v = %d\n", etats_x[i].var.v);
+        printf("\t Transitions sortantes :\n");
 
-        for (int j = 0; j < nb_trans_par_etat[s->etat]; j++) {
-            if (!transitions_x[i]) continue;
+        for (int j = 0; j < nb_trans_par_etat[etats_x[i].etat]; j++) {
             int id_cible = transitions_x[i][j].cible;
             int action_id = transitions_x[i][j].action_id;
-            printf("    [%d] --%s--> [%d]\n", i, actions[action_id], id_cible);
+            printf("\t\t   [%d] --%s--> [%d] \n",
+                   i,
+                   actions[action_id],
+                   id_cible);
         }
         printf("\n");
     }
 }
 
+// MAIN
 int main() {
     init_lts();
     init_variables();
     init_update_functions();
-    capacite_etats_x = 45;
+
+    capacite_etats_x = 100000000;
     init_lts_x();
     ajouter_etat_x(0, variable);
-
-
     appliquer_transitions();
+   // print_lts_x();
+    printf("Nombre total d'états étendus : %d\n", nb_etats_x);
 
-
-    print_lts_x();
-    printf("Total etats_x: %d\n", nb_etats_x);
     return 0;
 }
